3aaf5c013f3a06a3fba9c916067e4d94d1baf033
ybandil <yash.bandil@oracle.com>
2022-12-04 01:12:14 +0530

    new code change in routing

--- connector_test.py
+++ connector_test.py
@@ -61,5 +61,5 @@ def book():
 myconn.close()
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=8080)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+    #app.run(debug=True)
