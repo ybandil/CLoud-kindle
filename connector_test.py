347b3eabc36023ce2729bdce6eb117333e7d3cd2
ybandil <yash.bandil@oracle.com>
2022-12-05 03:50:08 +0530

    new code change in routing and pdf

--- connector_test.py
+++ connector_test.py
@@ -86,5 +86,5 @@ def image_route(filename):
 myconn.close()
 
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=8080)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+    #app.run(debug=True)
